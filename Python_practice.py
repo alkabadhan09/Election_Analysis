b86f7579790da89842b4b6d7154363b6cf00c776
alkabadhan09 <alkabadhan09@gmail.com>
2022-06-21 08:41:38 -0700

    Uploading practice file

---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+print ("Hello World")
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+if counties[2] != 'Jefferson':
+    print(counties[2])
+
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties.")
